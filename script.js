2f802c5c8cca7d3e4f2388d38ee464a06ecd6431
Bogdan Z <realer09@gmail.com>
2020-03-26 22:18:45 +0300

    feat: implement smooth on-click scrolling and active menu highlight

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ function slider(selector) {
 	const navRight = sliderElement.getElementsByClassName('nav_right')[0];
 
 	const slides = sliderElement.getElementsByClassName('slide');
+	for(const index in Object.keys(slides)) {
+		slides[index].style.left = `0px`;
+	}
 
 	let currentIndex = 1;
 
@@ -43,7 +46,69 @@ function togglePhoneScreen(...phoneDomIds) {
 	});
 }
 
+function initMenu(menuId, offsetElId) {
+	const menuEl = document.getElementById(menuId);
+	const anchorEls = menuEl.getElementsByTagName('a');
+
+	let activeAnchorEl = false;
+	
+	for (const i in Object.keys(anchorEls)) {
+		const sectionId = anchorEls[i].hash.replace('#', '');
+
+		anchorEls[i].addEventListener('click', function(e) {
+			e.preventDefault();
+			let newX = document.getElementById(sectionId).offsetTop;
+			newX -= document.getElementById(offsetElId).offsetHeight;
+			window.scrollTo({behavior: 'smooth', top: newX});
+		});
+	}
+
+	checkAndSetActiveMenuItem();
+
+	document.addEventListener('scroll', function(e) {
+		checkAndSetActiveMenuItem();
+	});
+
+	function checkAndSetActiveMenuItem() {
+		let scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+		scrollTop += document.getElementById(offsetElId).offsetHeight;
+
+		let newActiveAnchorEl = undefined;
+		let newActiveBlockOffset = -1;
+
+		const isBottom =  (window.innerHeight + window.scrollY) >= document.body.offsetHeight;
+
+		for (const i in Object.keys(anchorEls)) {
+			const candidate = anchorEls[i];
+			const sectionId = candidate.hash.replace('#', '');
+			const sectionBlock = document.getElementById(sectionId);
+			
+			if(!newActiveAnchorEl) {
+				newActiveAnchorEl = candidate;
+				newActiveBlockOffset = sectionBlock.offsetTop;
+				continue;
+			}
+			
+			if((sectionBlock.offsetTop <= scrollTop || isBottom) && sectionBlock.offsetTop > newActiveBlockOffset) {
+				newActiveAnchorEl =  candidate;
+				newActiveBlockOffset = sectionBlock.offsetTop;
+			}
+		}
+
+		if(!activeAnchorEl) {
+			newActiveAnchorEl.classList.add('active');
+			activeAnchorEl = newActiveAnchorEl; 
+		} else if(newActiveAnchorEl && newActiveAnchorEl !== activeAnchorEl) {
+			activeAnchorEl.classList.remove('active');
+			newActiveAnchorEl.classList.add('active');
+			activeAnchorEl = newActiveAnchorEl; 
+		}
+	}
+
+}
+
 window.onload = function() {
 	slider('phones_slider');
 	togglePhoneScreen('phone1', 'phone2', 'phone3');
+	initMenu('menu', 'home');
 };
